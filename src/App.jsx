fb6244520ae056e8fd63b1705862a37d7cde3a31
Oleksii Bolotin <samkiral73@gmail.com>
2023-04-04 10:43:49 +0200

    added / to movies and movies/:id

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:id" element={<MovieDetails />}>
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
